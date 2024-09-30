685998e46a8281e82c361d67a9aef832c3697eb7
vaibhav <vaibhavbhosale15@gmail.com>
2024-09-29 20:23:47 -0400

    fixing the installation scripts

--- remote-install.sh
+++ remote-install.sh
@@ -7,9 +7,9 @@ function RemoteExec() {
 
 function setupNode() {
     # check if the ssh folder exists
-    RemoteExec $1 'if [ ! -d ~/cloudlab-k8s-setup ];then git clone git@github.com:vaibhavb007/cloudlab-k8s-script.git; fi'
-    RemoteExec $1 'cd ~/cloudlab-k8s-setup && git pull'
-    RemoteExec $1 'bash ~/cloudlab-k8s-setup/local-install.sh'
+    RemoteExec $1 'if [ ! -d ~/cloudlab-k8s-script ];then git clone git@github.com:vaibhavb007/cloudlab-k8s-script.git; fi'
+    RemoteExec $1 'cd ~/cloudlab-k8s-script && git pull'
+    RemoteExec $1 'cd ~/cloudlab-k8s-script; bash ./local-install.sh'
 }
 
 python3 parser.py
@@ -19,7 +19,7 @@ for host in $(cat hosts.txt); do
     ssh-keyscan -H $host >> ~/.ssh/known_hosts
 
     RemoteExec $host 'if [ ! -d ~/.ssh ];then mkdir ~/.ssh; fi'
-    RemoteExec $host 'cp -r /proj/krios-PG0/.ssh/* ~/.ssh/'
+    RemoteExec $host 'if [ ! -f ~/.ssh/id_ed25519 ]; then cp -r /proj/krios-PG0/.ssh/* ~/.ssh/; fi'
     RemoteExec $host 'eval "$(ssh-agent -s)"'
     RemoteExec $host 'ssh-add ~/.ssh/id_ed25519'
 
