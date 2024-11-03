bc7356f94e4fe88d18fc289d632e4e1503e2e52a
Vaibhav <vbhosale6@gatech.edu>
2024-11-03 11:00:57 -0500

    updating k8s join contorl plane leader

--- k8s-join-control-plane.sh
+++ k8s-join-control-plane.sh
@@ -2,7 +2,7 @@
 
 function RemoteExec() {
     echo $2
-    # ssh -oStrictHostKeyChecking=no -p 22 "$1" "$2";
+    ssh -oStrictHostKeyChecking=no -p 22 "$1" "$2";
 }
 
 cmd = "sudo kubeadm join $2:6443 --token $3 --discovery-token-ca-cert-hash sha256:$4 --control-plane --certificate-key $5"
