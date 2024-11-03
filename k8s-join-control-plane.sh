0de40240bb0db1bd69f2c440a08d457c21a4dfde
vaibhavb <vaibhavb@node1.vaibhavb-227518.krios-pg0.utah.cloudlab.us>
2024-11-03 13:14:42 -0700

    k8s join control node updates

--- k8s-join-control-plane.sh
+++ k8s-join-control-plane.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function RemoteExec() {
-    echo $2
+    echo "$2"
     ssh -oStrictHostKeyChecking=no -p 22 "$1" "$2";
 }
 
-cmd = "sudo kubeadm join $2:6443 --token $3 --discovery-token-ca-cert-hash sha256:$4 --control-plane --certificate-key $5"
+join="sudo kubeadm join $2:6443 --token $3 --discovery-token-ca-cert-hash sha256:$4 --control-plane --certificate-key $5"
 
-RemoteExec $1 "$cmd"
+RemoteExec $1 "$join"
 
 RemoteExec $1 "sudo tc qdisc add dev eno1 root netem delay 0ms"
