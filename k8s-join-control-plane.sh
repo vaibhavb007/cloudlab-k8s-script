f3ed0d225fcd0df481a35099b32d436a7e0e8dfe
Vaibhav <vbhosale6@gatech.edu>
2024-11-03 11:00:28 -0500

    updating k8s join contorl plane leader

--- k8s-join-control-plane.sh
+++ k8s-join-control-plane.sh
@@ -2,11 +2,11 @@
 
 function RemoteExec() {
     echo $2
-    ssh -oStrictHostKeyChecking=no -p 22 "$1" "$2";
+    # ssh -oStrictHostKeyChecking=no -p 22 "$1" "$2";
 }
 
-cmd = "sudo kubeadm join 128.110.217.234:6443 --token u4nstj.tvvmgt9r4qepo29a --discovery-token-ca-cert-hash sha256:902954712e791a0135f914c3696804e1f010e5430e06086a5029ba950105d091 --control-plane --certificate-key $2"
+cmd = "sudo kubeadm join $2:6443 --token $3 --discovery-token-ca-cert-hash sha256:$4 --control-plane --certificate-key $5"
 
-RemoteExec $1 cmd
+RemoteExec $1 "$cmd"
 
 RemoteExec $1 "sudo tc qdisc add dev eno1 root netem delay 0ms"
